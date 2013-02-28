b521b3bdd836c8ac992779eeb7a17f8bee4b0ed9
Elvo <elvo@fantasymail.de>
2013-02-28 22:17:10 +0100

    erste Delphi-Datei

---
+++ ProjectTest.dpr
@@ -0,0 +1,13 @@
+program ProjectTest;
+
+uses
+  Forms,
+  UnitTest in 'UnitTest.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
